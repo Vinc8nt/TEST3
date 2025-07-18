59f4a133db4a3c5fa78d37b78341767b4fa433ca
Vinc8nt <earlvincent.victor09@gmail.com>
2025-07-18 11:27:49 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
+function scrollToInfo() {
+  document.getElementById('course-info').scrollIntoView({ behavior: 'smooth' });
+}
 
+function revealCircuits() {
+  const button = document.querySelector('.facility-button');
+  const container = document.querySelector('.circuit-container');
+  button.classList.add('move-side');
+  container.classList.remove('hidden');
+}
